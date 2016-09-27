8dcf755a50fafb894664e8a0ff209350fbf945d0
Phillip Lorenzo <pllearns@gmail.com>
2016-09-27 09:39:12 -0700

    WIP: Created a player class that contains clothes and cash, still blocked on how we can implement this into the betting system, and how player collects money when winning

--- src/betting.js
+++ src/betting.js
@@ -1,1 +1,31 @@
-const bettingPool = 10
+import currency from './currency'
+
+const CASH = [
+  { type: '💵', value: 10}
+]
+
+const CLOTHES = [
+  { type: '👕', value: 'shirt'}
+  { type: '👞', value: 'shirt'}
+  { type: '👖', value: 'shirt'}
+]
+
+class Account {
+  constructor( cash, clothes ) {
+    this.cash = cash
+    this.clothes = clothes
+  }
+
+  toString() {
+    return `${this.cash.symbol} ${this.displayClothes()}`
+  }
+
+  displayClothes() {
+    if( this.clothes.type === '👕')
+    return 'shirt'
+  } else if ( this.clothes.type === '👞'){
+    return 'shoes'
+  } else if ( this.clothes.type === '👖'){
+    return 'pants'
+    }
+}
